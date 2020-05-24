135e92195f33eeae72a038804fcc353fbd3a2789
zeroclock <zeroclock@icloud.com>
2020-05-24 19:54:13 +0900

    Add reading config file feature

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,46 @@ use std::fs;
 use std::path::Path;
 use headless_chrome::{Browser, protocol::page::*};
 use headless_chrome::protocol::target::methods::CreateTarget;
+use serde::{Serialize, Deserialize};
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Config {
+    images: String,
+    watch: String,
+    url: String,
+    settings: Vec<IndivisualSetting>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct IndivisualSetting {
+    name: String,
+    path: String,
+}
+
+impl Config {
+    fn new(path: Option<&str>) -> Result<Config> {
+        let p: &str;
+        match path {
+            Some(str) => p = str,
+            None => p = ".quokkaconfig.json",
+        }
+        let file = fs::File::open(p)?;
+        let reader = std::io::BufReader::new(file);
+        let config = serde_json::from_reader(reader)?;
+        Ok(config)
+    }
+}
+
+impl IndivisualSetting {
+    fn new(name: &str, path: &str) -> IndivisualSetting {
+        IndivisualSetting {
+            name: name.to_string(),
+            path: path.to_string(),
+        }
+    }
+}
 
 pub fn get_images(path: &str) -> Result<Vec<DynamicImage>> {
     let paths = fs::read_dir(path)?;
@@ -54,11 +92,10 @@ fn take_screenshot(width: i32, height: i32, url: &str) -> anyhow::Result<Vec<u8>
         None,
         true
     )?;
-   
+
     Ok(screenshot)
 }
 
-// GRCOV_EXCL_START
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -86,7 +123,6 @@ mod tests {
         assert_eq!(true, images.is_err());
     }
 
-
     #[test]
     fn test_take_screenshot() {
         let image = take_screenshot(1920, 1080, "https://google.com");
@@ -94,5 +130,32 @@ mod tests {
         let image = take_screenshot(1920, 1080, "invalidurl");
         assert_eq!(true, image.is_err());
     }
+
+    #[test]
+    fn test_new_indivisual_setting() {
+        let expected = IndivisualSetting {
+            name: "index_pc.png".to_string(),
+            path: "/".to_string(),
+        };
+        assert_eq!(expected, IndivisualSetting::new("index_pc.png", "/"));
+    }
+
+    #[test]
+    fn test_new_config() {
+        let expected = Config {
+            images: "design_camp".to_string(),
+            watch: "src".to_string(),
+            url: "127.0.0.1:9001".to_string(),
+            settings: vec![
+                IndivisualSetting::new("index_pc.png", "/"),
+                IndivisualSetting::new("index_sp.png", "/"),
+                IndivisualSetting::new("posts_pc.png", "/posts"),
+                IndivisualSetting::new("posts_sp.png", "/posts"),
+            ]
+        };
+        let actual = Config::new(Some("test/testconfig.json"));
+        assert_eq!(true, actual.is_ok());
+        assert_eq!(expected, actual.unwrap());
+        assert_eq!(true, Config::new(None).is_err());
+    }
 }
-// GRCOV_EXCL_STOP
