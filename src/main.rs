6c78b768890d9068f1d51bcbb6e3d72cbc77bbf9
zeroclock <zeroclock@icloud.com>
2020-05-30 12:43:28 +0900

    Add fs watch and server

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,48 @@
 extern crate quokka;
 
-use notify::{Watcher, RecommendedWatcher, RecursiveMode};
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use quokka::*;
+use std::sync::mpsc;
+use std::thread;
+use warp::{http::Uri, Filter};
 
 // GRCOV_EXCL_START
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let config = Config::new(None)?;
+    let (tx, rx) = mpsc::channel();
 
-    let mut watcher: RecommendedWatcher = Watcher::new_immediate(|res| {
+    let mut watcher: RecommendedWatcher = Watcher::new_immediate(move |res| {
         match res {
-            Ok(event) => println!("event: {:?}", event),
-            Err(e) => println!("watch error: {:?}", e),
+            Ok(event) => tx.send(format!("event: {:?}", event)).unwrap(),
+            Err(e) => tx.send(format!("watch error: {:?}", e)).unwrap(),
         }
     })?;
 
+    // watcher.configure(notify::Config::OngoingEvents(Some(
+    //     std::time::Duration::from_millis(500),
+    // )));
     watcher.watch(&config.watch, RecursiveMode::Recursive)?;
+
+    let body = r#"
+<html>
+    <head>
+        <title>HTML with warp!</title>
+    </head>
+    <body>
+        <h1>Response Test</h1>
+    </body>
+</html>
+"#;
+
+    let hello = warp::path::end()
+        .map(move || warp::reply::html(body));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    loop {
+        let event = rx.recv().unwrap();
+        println!("Got: {:?}", event);
+    }
 
     Ok(())
 }
